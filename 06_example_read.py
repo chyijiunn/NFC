83eed7d7ca5736800208ffc023bc89cf303e379e
chyijiunn <chyijiunn@gmail.com>
2022-11-15 11:45:48 +0800

    fix
    fix

--- 06_example_read.py
+++ 06_example_read.py
@@ -1,12 +1,8 @@
 from py532lib.mifare import *
 import time
-card = Mifare()
-card.SAMconfigure()
-card.set_max_retries(MIFARE_SAFE_RETRIES)
-
-uid = card.scan_field()
+uid = Mifare().scan_field()
 if uid:                                 # 如果 uid 有掃到，回傳 True ，執行下面一次
-    address = card.mifare_address(i,j)  # i = 0 ~ 3 section ; j = 0 ~ 3 block，回傳 address
-    data = card.mifare_read(address)    # 讀取該 address 資料 
+    address = Mifare().mifare_address(0,0)  # i = 0 ~ 3 section ; j = 0 ~ 3 block，回傳 address
+    data = Mifare().mifare_read(address)    # 讀取該 address 資料 
     print(bytes(data))
         
--- 07_add.py
+++ 07_add.py
@@ -1,17 +1,14 @@
 from py532lib.mifare import *
 import time , binascii
-card = Mifare()
-card.SAMconfigure()
-card.set_max_retries(MIFARE_SAFE_RETRIES)
-uid = card.scan_field()
+uid = Mifare().scan_field()
 i = 0
 if uid:
     ID = binascii.hexlify(uid)
     print('ID:',ID)
     for section in range(2):    #讀 section = 0 , 1 的區域
         for block in range(4):  #讀 block = 0 ~ 3 的位置
-            address = card.mifare_address(section,block)
-            data = card.mifare_read(address)
+            address = Mifare().mifare_address(section,block)
+            data = Mifare().mifare_read(address)
             print(i,':',bytes(data))
             i += 1              #讀取第幾次-1
         

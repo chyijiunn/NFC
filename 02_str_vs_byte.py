f5ba2cfbae6800f0a735841e7b4637745bd3c8ec
chyijiunn <chyijiunn@gmail.com>
2022-11-15 10:25:28 +0800

    ud
    ud

--- 02_str_vs_byte.py
+++ 02_str_vs_byte.py
@@ -1,11 +1,14 @@
-string_1 = 'Peter'
-string_2 = b'Peter'
+string_1 = 'Peter'  #字串
+string_2 = b'Peter' #位元
+
+#以下比較兩者差別
 
 print(string_1 , ':',type(string_1))
 for i in string_1:
     print(i)
 
 print('\n')
+
 print(string_2 ,':', type(string_2))
 for j in string_2:
     print(j)

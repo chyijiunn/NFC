8a5c7cc5f94db330ec0a7486e50c4a8caea1ea56
chyijiunn <chyijiunn@gmail.com>
2022-11-15 08:36:56 +0800

    updated
    updated

--- 12_tag_delet.py
+++ 12_tag_delet.py
@@ -10,7 +10,7 @@ while True:
         ID = binascii.hexlify(Mifare().scan_field())
         hs_ori = hashlib.md5(ID)
         hs_ori_md5 = hs_ori.hexdigest()
-        if hs_ori_md5 == 'c8c108778b83980115c820f51fbd087d':
+        if hs_ori_md5 == 'c8c108778b83980115c820f51fbd087d':#這裡是雜湊值
             print(i,':','open')
         else:print(i,':','No!')
         time.sleep(1)

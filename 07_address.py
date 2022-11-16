b678a0374c770e0a69a41be487544e487548e173
chyijiunn <chyijiunn@gmail.com>
2022-11-16 08:42:53 +0800

    ud
    ud

--- 07_address.py
+++ 07_address.py
@@ -4,12 +4,12 @@ uid = Mifare().scan_field()
 i = 0
 if uid:
     ID = binascii.hexlify(uid)
-    print('ID:',ID)
-    for section in range(2):    #讀 section = 0 , 1 的區域
+    print('ID:',ID,':',uid)
+    for section in range(3):    #讀 section = 0 , 1 的區域
         for block in range(4):  #讀 block = 0 ~ 3 的位置
             address = Mifare().mifare_address(section,block)
             data = Mifare().mifare_read(address)
-            print(i,':',bytes(data))
+            print(i,'(',section,block,'):',bytes(data))
             i += 1              #讀取第幾次-1
         
 
